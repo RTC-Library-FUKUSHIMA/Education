c0a4bc1281bf68fd47498fa5324e8db170d5529f
Tsuyoshi Anazawa <30171705+anazawa-t@users.noreply.github.com>
2021-03-08 10:55:26 +0900

    Rename the executable file.

--- ChoreonoidWorkshop/TurtleBot2/plot/plot.sh
+++ ChoreonoidWorkshop/TurtleBot2/plot/plot.sh
@@ -3,5 +3,5 @@
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-#g++ ${cwd}/plot.cpp -I/usr/include/python2.7 -lstdc++ -lpython2.7 -std=c++11 && ${cwd}/a.out
-g++ ${cwd}/plot.cpp -I/usr/include/python3.6m -lstdc++ -lpython3.6m -std=c++11 && ${cwd}/a.out
+#g++ ${cwd}/plot.cpp -I/usr/include/python2.7 -lstdc++ -lpython2.7 -std=c++11 -o plot && ${cwd}/plot
+g++ ${cwd}/plot.cpp -I/usr/include/python3.6m -lstdc++ -lpython3.6m -std=c++11 -o plot && ${cwd}/plot
